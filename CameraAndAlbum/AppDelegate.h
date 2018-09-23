775417e19e20cc07736eee25ce0aa4e9ff230d5b
Zhang Van <tobeabettermanzxh91@gmail.com>
2018-09-23 16:21:20 +0800

    Initial Commit

---
+++ CameraAndAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraAndAlbum
+//
+//  Created by Mac on 2018/9/23.
+//  Copyright © 2018 VanZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraAndAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraAndAlbum
+//
+//  Created by Mac on 2018/9/23.
+//  Copyright © 2018 VanZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
